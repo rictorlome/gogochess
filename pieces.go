f728ce596fe42c65f50a53f5c4e5308aaebb9dfa
rictorlome <rictorlome@gmail.com>
2018-06-13 21:18:31 -0400

    Generate map of positions to pieces based on fen string

--- pieces.go
+++ pieces.go
@@ -25,22 +25,46 @@ var whiteSymbols = map[string]bool{
   "K": true,
   "P": true,
 }
+var blackSymbols = map[string]bool{
+  "r": true,
+  "n": true,
+  "b": true,
+  "q": true,
+  "k": true,
+  "p": true,
+}
 
-// var whitePieces = map[Position]Piece
-// var blackPieces = map[Position]Piece
+var whites map[Position]string
+var blacks map[Position]string
 
 func main() {
   fen := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
   fields := strings.Split(fen, " ")
   pos := fields[0]
   rows := strings.Split(pos, "/")
-  for _, row := range rows {
-    // offset := 0
-    for _, sq := range row {
-      _, found := whiteSymbols[string(sq)]
-      fmt.Println(found)
+
+  whites = make(map[Position]string)
+  blacks = make(map[Position]string)
+
+  for i, row := range rows {
+    offset := 0
+    for j, sq := range row {
+      _, white := whiteSymbols[string(sq)]
+      _, black := blackSymbols[string(sq)]
+      if white {
+        whites[Position{7-i,j+offset}] = string(sq)
+        continue
+      }
+      if black {
+        blacks[Position{7-i,j+offset}] = string(sq)
+        continue
+      }
+      // This gives the numeric value of the rune(ie '56' to the int 8)
+      offset += int(sq - '0')
     }
   }
+  fmt.Println(whites)
+  fmt.Println(blacks)
 
   // p := Position{1,1}
   // b := Board{}
