45ec499bde66e41abee2fb47b1d028f6c6aa0351
rictorlome <rictorlome@gmail.com>
2018-07-21 18:55:44 -0400

    Remove old api code; add radio switch for which method is being tested

--- api.go
+++ api.go
@@ -18,7 +18,6 @@ func startServer() {
   methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
   origins := handlers.AllowedOrigins([]string{"*"})
   router.HandleFunc("/tests", TestEndpoint).Methods("GET","POST")
-  router.HandleFunc("/GetMoves", GetNextMoves).Methods("GET")
   if err := http.ListenAndServe(":8080", handlers.CORS(headers, methods, origins)(router)); err != nil {
     panic(err)
   }
@@ -46,9 +45,9 @@ func HandleTestPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostNextMoves(w http.ResponseWriter, r *http.Request) {
-  //params are fen, square, PostType
+  //params are fen, square, methodToTest, PostType
   r.ParseForm()
-  NextMoves := GetPieceMoves(r.Form["fen"][0], r.Form["square"][0])
+  NextMoves := GetPieceMoves(r.Form["fen"][0], r.Form["square"][0], r.Form["methodToTest"][0])
   json.NewEncoder(w).Encode(NextMoves)
 }
 
@@ -72,27 +71,17 @@ type PieceMoves struct {
   Moves []MoveStatus
 }
 
- func GetNextMoves(w http.ResponseWriter, r *http.Request) {
-  q := Queen{}
-  q.isWhite = true
-  pmt := []PieceMoves {
-      GetPieceMoves("rnbqkbnr/ppp2ppp/3p4/3Qp3/4P3/1PN5/P1PP1PPP/R1B1KBNR w KQkq - 0 1", "d5"),
-      GetPieceMoves("rnbqkbnr/ppp2ppp/3p4/3Qp3/4P3/1PN5/P1PP1PPP/R1B1KBNR w KQkq - 0 1", "c3"),
-      GetPieceMoves("rnbqkbnr/ppp2ppp/3p4/3Qp3/4P3/1PN5/P1PP1PPP/R1B1KBNR w KQkq - 0 1", "e4"),
-      GetPieceMoves("rnbqkbnr/ppp2ppp/3p4/3Qp3/4P3/1PN5/P1PP1PPP/R1B1KBNR w KQkq - 0 1", "b7"),
-      GetPieceMoves("rnbqkbnr/ppp2ppp/3p4/3Qp3/4P3/1PN5/P1PP1PPP/R1B1KBNR w KQkq - 0 1", "f1"),
-      GetPieceMoves("rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 1", "e5"),
-      GetPieceMoves("rnb1kb1r/ppp1p1pp/3q1n2/3pPp2/3P4/8/PPP2PPP/RNBQKBNR w KQkq - 0 1", "e5"),
-      // GetPieceMoves("r1bqk2r/pppp1ppp/2n2n2/2b1p3/2B1P3/2P2N2/PP1P1PPP/RNBQK2R w KQkq - 0 1", "e1"),
-  }
-  json.NewEncoder(w).Encode(pmt)
-}
-
-func GetPieceMoves(fen string, sq string) PieceMoves {
+func GetPieceMoves(fen string, sq string, methodToTest string) PieceMoves {
   b := GenerateBoard(fen)
   pos := ToPos(sq)
   _, piece := b.findPiece(pos)
-  moves := piece.GetPseudoLegalMoves(pos, &b)
+  var moves map[Position]bool
+  switch methodToTest {
+  case "GetAttackingSquares":
+    moves = piece.GetAttackingSquares(pos, &b)
+  case "GetPseudoLegalMoves":
+    moves = piece.GetPseudoLegalMoves(pos, &b)
+  }
   var sqs []MoveStatus
   for move, _ := range(moves) {
     sq := MoveStatus{move.String(), b.wouldCauseCheck(parseMove(sq+move.String()))}
